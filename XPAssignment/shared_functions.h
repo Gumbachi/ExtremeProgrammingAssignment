78437252fc5cd576ae5d9dd57be9051026313c04
Gumbachi <jaredremsberg@gmail.com>
2022-04-15 11:36:19 -0400

    added a get number input loop

--- XPAssignment/shared_functions.h
+++ XPAssignment/shared_functions.h
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string>
 
 using namespace std;
 
@@ -18,9 +19,26 @@ bool drewSpecialFeature();
 // Can change the type of function depending on what your thing does.
 bool jaredSpecialFeature();
 
+// Check if a number is prime
+bool isPrime(int num) {
+    // 0 and 1 are not prime numbers
+    if (num == 0 || num == 1) {
+        return false;
+    }
+
+    // loop to check if n is prime
+    for (int i = 2; i <= num / 2; ++i) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
 // Types can be changed based on the judgement of the feture implementer.
-int hotAndColdGuess();
-int disvisibiltyCheck();
+bool disvisibiltyCheck(int num, int divisor) {
+    return num % divisor == 0;
+}
 
 string processHighOrLowGuess(int num, int guess) {
     // TODO
@@ -39,21 +57,44 @@ int generateNumber(){
     return number;
 }
 
+int getNumberInput(string prompt) {
+    int guess = -1;
+
+    while (true) {
+        cout << prompt << " >> ";
+
+        try {
+            cin >> guess;
+        } catch (int e) {
+            continue;
+        }
+        break;
+    }
+    return guess;
+}
+
 //Is called in every instance of the main loop.
 void guessingGameLoop (int count){
     int playerChoice, divisibiltyOption;
     int lower, upper;
     int newRange = -1;
+    int count = 0;
     bool validChoice = false;
     if (count == 0){
         cout << "I'm thinking of a number between " << LOWER << "and" << UPPER << endl << endl;
         cout << "How would you like to narrow it down? \n\t";
-        lower = LOWER; upper = UPPER;
     }
-    else
+    else {
         cout << "Your current options:\n";
-    cout << "1) Guess a number. If wrong, I'll tell ya 'hot' or 'cold'!\n" << "2) Check the number's divisibilty.\n";
+    }
+
+    cout << "1) Guess a number. If wrong, I'll tell ya 'higher' or 'lower'!\n";
+    cout << "2) Check the number's divisibilty.\n";
+    cout << "3) Check if the number is prime.\n";
+    cout << "4) Reveal possible last digits of the number.\n";
     cout << "Currently Known Info: \n" << "Known Range: " << LOWER << " to " << UPPER;
+        
+    
 
     if (count >= 4)
         drewTextDisplay();
